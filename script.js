1973f06d1b80e8fa1fa429c70a5b9c3c033ad119
gokul kumar <gokul.prajapat.jnvu@gmail.com>
2023-01-25 14:26:58 +0530

    style added

--- script.js
+++
@@ -1,8 +0,0 @@
-function myfunction(){
-    let nav=document.querySelector('.topnav');
-    if(nav.className === 'topnav'){
-        nav.className+=' responsive';
-    }else{
-        nav.className='topnav';
-    }
-}
